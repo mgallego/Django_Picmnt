680567343a7393ac9e2e78173b7b19e57cc098f1
Moises Gallego <moisesgalleo@gmail.com>
2012-05-27 12:56:56 +0200

    added less support

--- django_picmnt/settings.py
+++ django_picmnt/settings.py
@@ -49,18 +49,18 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = '/var/mediaroot/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/home/moises/Dev/Exp/django_picmnt/picmnt/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -79,6 +79,8 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    'compressor.finders.CompressorFinder',
+
 )
 
 # Make this unique, and don't share it with anybody.
@@ -120,6 +122,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'compressor',
     'picmnt',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
@@ -155,3 +158,11 @@ LOGGING = {
         },
     }
 }
+
+COMPRESS_PRECOMPILERS = (
+   ('text/less', 'lessc {infile} {outfile}'),
+)
+
+INTERNAL_IPS = ('127.0.0.1',)
+
+COMPRESS_ROOT = STATIC_ROOT
