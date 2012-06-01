a07c2ef051f8d4bb4e14d3ad20aea1a0415c6b0e
Moises Gallego <moisesgalleo@gmail.com>
2012-06-02 00:00:44 +0200

    add tests

--- django_picmnt/urls.py
+++ django_picmnt/urls.py
@@ -15,7 +15,8 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
-                       (r'^$', main_page),
+                       #(r'^$', main_page),
+                       (r'^$', random_image), #temp, for test 
                        (r'^random/$', random_image),
 
 
--- picmnt/tests.py
+++ picmnt/tests.py
@@ -1,16 +1,25 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+from django.test import TestCase, LiveServerTestCase
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
+import time
+from pyvirtualdisplay import Display
+import os
 
-Replace this with more appropriate tests for your application.
-"""
+class ImageTest(LiveServerTestCase):
 
-from django.test import TestCase
+    def setUp(self):
+        self.SERVER = 'http://picmnt.dev'
+        self.display = Display(visible=0, size=(800, 600))
+        self.display.start()
+        self.browser = webdriver.Firefox()
 
+    def tearDown(self):
+        self.browser.quit()
+        self.display.stop()
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def test_random_image(self):
+        self.browser.get(self.SERVER + '/random')
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Home', body.text)
+        
